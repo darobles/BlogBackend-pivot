75ed65676b4e66c28bfff6a51a191baacbb2ca23
darobles <darobles@users.noreply.github.com>
2025-04-14 06:48:57 +0000

    deploy: TaskMaster-New-Zealand/BlogBackend@91209439a92f0de0beefe3685de93d84a6176786

--- core/settings.py
+++ core/settings.py
@@ -172,3 +172,11 @@ REST_FRAMEWORK = {
 
 # CORS settings
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Cache settings
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
